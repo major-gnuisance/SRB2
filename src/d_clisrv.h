589b030a3897b5bd9ab8c72cb9e0a9b67678442e
Louis-Antoine <lamr@free.fr>
2018-03-02 22:51:00 +0100

    Add missing declaration for cv_maxdownloads

--- src/d_clisrv.h
+++ src/d_clisrv.h
@@ -460,7 +460,7 @@ extern UINT32 realpingtable[MAXPLAYERS];
 extern UINT32 playerpingtable[MAXPLAYERS];
 #endif
 
-extern consvar_t cv_joinnextround, cv_allownewplayer, cv_maxplayers, cv_resynchattempts, cv_blamecfail, cv_maxsend, cv_noticedownload, cv_downloadspeed;
+extern consvar_t cv_joinnextround, cv_allownewplayer, cv_maxplayers, cv_resynchattempts, cv_blamecfail, cv_maxsend, cv_noticedownload, cv_downloadspeed, cv_maxdownloads;
 extern consvar_t cv_extranotices;
 extern consvar_t cv_refusegay; // !!!
 
